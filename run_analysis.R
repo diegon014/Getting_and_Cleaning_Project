53c887d50af27f9545a1620214c8b2e3d7e2bf1e
Diego Nuñez <dlnunez@gmail.com>
2021-06-27 20:49:51 -0300

    Init

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+
+#Read Test Data
+y_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+
+
+#Read Training Data
+y_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
