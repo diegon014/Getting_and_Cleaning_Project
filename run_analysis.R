4c90b63ea1e13528165243bf1a8eafe52747dabe
Diego Nuñez <dlnunez@gmail.com>
2021-06-28 20:31:34 -0300

    Final

--- run_analysis.R
+++ run_analysis.R
@@ -15,6 +15,9 @@ subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 #Read Labels
 labels <- read.table("./UCI HAR Dataset/features.txt")
 
+#Read Activity names
+activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
 #Columns Names
 colnames(x_test) <- labels[,2]
 colnames(x_train) <- labels[,2]
@@ -30,6 +33,30 @@ train_data <- cbind(subject_train, y_train, x_train)
 full_data <- rbind(test_data, train_data)
 
 
-#Convert to table
-point_2_data <- select(full_data,contains(c("mean","std")))
+#Extract the measurements on the mean and standard deviation
+full_data <- select(full_data, SubjectId, ActivityId, contains(c("mean","std")))
+
+
+#Replace ActivityId with Activity Description
+full_data$ActivityId <- activity[full_data$ActivityId,2]
+
+
+#Replace Columns Names with appropriate descriptive names
+names(full_data)<-gsub("^t", "Time", names(full_data))
+names(full_data)<-gsub("^f", "Frequency", names(full_data))
+names(full_data)<-gsub("-mean()", "Mean", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-std()", "STD", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-mad()", "Median", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-max()", "Largest", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-min()", "Small", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-sma()", "SignalMagnitude", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-energy()", "Energy", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-iqr()", "Interquartile", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("-entropy()", "SignalEntropy", names(full_data), ignore.case = TRUE)
+names(full_data)<-gsub("angle", "Angle", names(full_data))
+
+#Calculate the Mean of each measure
+final_data <- full_data %>%
+        group_by(ActivityId,SubjectId) %>%
+        summarise_all(funs(mean))
 
